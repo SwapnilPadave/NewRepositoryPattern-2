164560c96dff5051b3f24293e6ef52f03f7db801
SwapnilPadave <Spadave7@gmail.com>
2022-07-21 17:03:42 +0530

    a

--- NewRepo2/Controllers/LoginController.cs
+++ NewRepo2/Controllers/LoginController.cs
@@ -1,12 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using NewRepo2.DTO.LoginDTO;
 using NewRepo2.DTO.RolesDTO;
 using NewRepo2.Service.IServices;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace NewRepo2.Controllers
